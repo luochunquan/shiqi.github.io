13e56717684e290d52b4cdc2d99acb1b3b43c120
LuoChunQuan <1165012317@qq.com>
2025-03-17 17:46:34 +0800

    111

--- versionconfig.js
+++ versionconfig.js
@@ -1,6 +1,6 @@
 
-      const VUE_APP_VERSION = 'V 1.4.0 250314' + getAPKVersion();
-      const VUE_APP_VTIME = '1741943557978';
+      const VUE_APP_VERSION = 'V 1.4.0 250317' + getAPKVersion();
+      const VUE_APP_VTIME = '1742204631777';
       const VUE_APP_FONT_FAMILY_NAME = {"fontFilesList":[{"fontFamily":"SofiaPro","fontUrl":"/SofiaPro-Black.900.6a22fe9b72bbc6ab.otf","display":"swap","style":"normal","weight":"900"},{"fontFamily":"SofiaPro","fontUrl":"/SofiaPro-Bold.700.6c051b3c69a84211.otf","display":"swap","style":"normal","weight":"700"},{"fontFamily":"SofiaPro","fontUrl":"/SofiaPro-Medium.500.6bcefc485576e059.otf","display":"swap","style":"normal","weight":"500"},{"fontFamily":"SofiaPro","fontUrl":"/SofiaPro-Regular.400.705232bdeb996c5e.otf","display":"swap","style":"normal","weight":"400"},{"fontFamily":"SofiaPro","fontUrl":"/SofiaPro-SemiBold.600.833443548c9dc9dc.otf","display":"swap","style":"normal","weight":"600"}],"gameFontFilesList":[{"fontFamily":"fontGame","fontUrl":"/FontGame4.894b89c344313599.ttf","display":"swap","style":"normal"}]};
       function getAPKVersion(){
         if (!window.android) {
@@ -10,10 +10,10 @@
         return '-' + window.android.callJava('getApkVersion', strJson);
       }
         window.addEventListener('load', function () {
-            document.getElementById('versionIds').innerHTML = "V 1.4.0 250314" + getAPKVersion();
-            const stylesheets = ["./1741943513370z4ggigws5k/app.7d31805f.css","./1741943513370z4ggigws5k/chunk-commons.166ad437.css","./1741943513370z4ggigws5k/chunk-dialog.b6f94865.css","./1741943513370z4ggigws5k/chunk-vuetify.5ae2192a.css"];
-            const scripts = ["./1741943513370z4ggigws5k/app.77c2d887.js","./1741943513370z4ggigws5k/chunk-commons.77c2d887.js","./1741943513370z4ggigws5k/chunk-dialog.77c2d887.js","./1741943513370z4ggigws5k/chunk-libs.77c2d887.js","./1741943513370z4ggigws5k/chunk-view.77c2d887.js","./1741943513370z4ggigws5k/chunk-vuetify.77c2d887.js","./1741943513370z4ggigws5k/manifest.77c2d887.js"];
-            const libs = ["./1741943513370z4ggigws5k/vue-i18n.min.js"];
+            document.getElementById('versionIds').innerHTML = "V 1.4.0 250317" + getAPKVersion();
+            const stylesheets = ["./1742204585060qpuus82jbyb/app.78f11262.css","./1742204585060qpuus82jbyb/chunk-commons.2be443be.css","./1742204585060qpuus82jbyb/chunk-dialog.026cd35b.css","./1742204585060qpuus82jbyb/chunk-vuetify.5ae2192a.css"];
+            const scripts = ["./1742204585060qpuus82jbyb/app.19eb0661.js","./1742204585060qpuus82jbyb/chunk-commons.19eb0661.js","./1742204585060qpuus82jbyb/chunk-dialog.19eb0661.js","./1742204585060qpuus82jbyb/chunk-libs.19eb0661.js","./1742204585060qpuus82jbyb/chunk-view.19eb0661.js","./1742204585060qpuus82jbyb/chunk-vuetify.19eb0661.js","./1742204585060qpuus82jbyb/manifest.19eb0661.js"];
+            const libs = ["./1742204585060qpuus82jbyb/vue-i18n.min.js"];
 
 			const loadlist = [...libs, ...scripts, ...stylesheets];
 			loadlist.forEach(src => {
